9339e8d193216a3e49726cc58cd32d9ffb82e89d
sahil-gusain <100952277+sahil-gusain@users.noreply.github.com>
2022-09-14 09:42:17 +0530

    Add files via upload

---
+++ CBNST/Regula-falsi.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+
+float fun(float x,int x1,int x2,int x3)
+{
+	return (pow(x,x1)+(x2*x)+x3);
+}
+
+int main()
+{   int m,n,x1,x2,x3;
+    float a,b;
+    
+    printf("enter the power and  coefficient ");
+    scanf("%d %d %d",&x1,&x2,&x3);
+ 
+	printf("enter the range ");
+	scanf("%d %d",&m,&n);
+	for(float i=m;i+1<n;i++){
+		if(fun(i,x1,x2,x3)*fun(i+1,x1,x2,x3)<0)
+		{
+			a=i;
+			b=(i+1);
+			break;
+		}
+	}
+	printf("a is %f and b is %f\n",a,b);
+	int i=1;
+	float x=	x=((a*fun(b,x1,x2,x3))-(b*fun(a,x1,x2,x3)))/(fun(b,x1,x2,x3)-fun(a,x1,x2,x3));
+	do{
+		printf("value at iteration x%d is %f\n ",i,x);
+	
+		if(fun(a,x1,x2,x3)*fun(x,x1,x2,x3)<0) b=x;
+		else if(fun(x,x1,x2,x3)*fun(b,x1,x2,x3)<0) a=x;
+		
+		x=((a*fun(b,x1,x2,x3))-(b*fun(a,x1,x2,x3)))/(fun(b,x1,x2,x3)-fun(a,x1,x2,x3));
+		i++;
+    }while(fabs(x-a)>=0.000001 && fabs(x-b)>=0.000001);
+	
+}
+
